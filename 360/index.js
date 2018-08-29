4f11ef6b005ac08ac1b2052dd38bf2febab82e57
victor <vrecinesm@gmail.com>
2018-08-29 07:05:01 -0500

    ok

--- 360/index.js
+++ 360/index.js
@@ -4,24 +4,46 @@ var CompetitionOpens = CompetitionRenderer.querySelectorAll(".ipo-Competition.ip
 for(var i=0;i<CompetitionOpens.length;i++){
     var title = CompetitionOpens[i].querySelector(".ipo-CompetitionButton .ipo-CompetitionButton_NameLabel").innerText;
     var games = CompetitionOpens[i].querySelectorAll(".ipo-Fixture");
-    console.log("==============================:" + title);
+
     for(var e=0;e<games.length;e++){
         var player = games[e].querySelectorAll(".ipo-TeamStack .ipo-TeamStack_TeamWrapper");
 
         var teamScore = games[e].querySelectorAll(".ipo-SetScore .ipo-SetScore_SetWrapper");
-        var teamPoints = games[e].querySelectorAll(".ipo-TeamPoints");
+        var teamPoints = games[e].querySelector(".ipo-TeamPoints");
 
         var gaming1 = player[0].innerText;
         var gaming2 = player[1].innerText;
-        var setname = player[2].innerText;
+        var game = player[2].innerText;
+        var sets = teamScore.length;
 
-        console.log("gaming1",gaming1);
-        console.log("gaming2",gaming2);
-        console.log("setname",setname);
+        var turn1 = false;
+        var turn2 = false;
+        var score1 = [];
+        var score2 = [];
+        var point1 = 0;
+        var point2 = 0;
 
-        console.log("teamScore",teamScore);
-        console.log("teamPoints",teamPoints);
+        for(var j=0;j<sets;j++){
+            var scores = teamScore[j].querySelectorAll(".ipo-SetScore_SetResult");
+            score1.push(scores[0].innerText);
+            score2.push(scores[1].innerText);
+        }
 
-        console.log("#");
+        var points = teamPoints.querySelectorAll(".ipo-TeamPoints_TeamScore");
+        point1 = points[0].innerText;
+        point2 = points[1].innerText;
+
+        addApuesta(title, game, sets,{
+            turn: turn1,
+            name: gaming1,
+            scores: score1,
+            points: point1
+        },{
+            turn: turn2,
+            name: gaming2,
+            scores: score2,
+            points: point2
+        });
+
     }
 }
---
+++ 360/index2.js
@@ -0,0 +1,30 @@
+/**
+ * Created by victor167 on 28/08/2018.
+ */
+
+var championships = [];
+var games = [];
+
+function addApuesta(title,game,sets,object1,object2){
+    /**
+     * title="",
+     * game=""
+     * object={
+     *      turn: true
+     *      name: "Victor",
+     *      scores:[4,7,3]
+     *      points: 40
+     * }
+     */
+    console.log("=================");
+    console.log("title",title);
+    console.log("game",game);
+    console.log("sets",sets);
+    console.log("object1",object1);
+    console.log("object2",object2);
+    console.log("=================");
+}
+
+function addChampionship(title){
+
+}
